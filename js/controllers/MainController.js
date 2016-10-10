dddc607138360b527a40582fbe0c71cbe30aa664
Martin Verdier <mart.verdier@gmail.com>
2016-10-10 09:44:27 +0200

    Added a ton of docs to fill in your data

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -10,9 +10,22 @@ app.controller('MainController', ['$scope', '$http', function($scope, $http) {
 		"phoneDisplay": "(+33) 6 24 72 64 58", //Your phone for the display, the one that a human can easily read!
 		"email": "mart.verdier@gmail.com", //Your email
 		"picture": "resources/profile.png", //The relative path to your picture in the project's folder (relative to resume.html)
-		"github": "mverdier" //Your GitHub link, is you have one
+		"github": "mverdier" //Your GitHub link, if you have one
 	};
 
+	/*
+	This is your school data. In there you'll put the several degrees you've earned,
+	as well as your certifications or stuff like that.
+	
+	The format is pretty straight-forward, each element has a title, dates, and a location:
+	{
+		"title": "Your degree, like Bachelor Degree in Computer Science",
+		"dates": "The dates between which you workd on that degree, like 2010-2013",
+		"location": "Which establishment gave it to you, for example Massachusetts Institute of Technology, Boston, MA"
+	}
+	
+	That's it. You can fill those and put as many as you like.
+	*/
 	$scope.school = [{
 		"title": "Baccalauréat Scientifique (Engineer Science Option), mention AB",
 		"dates": "2012-2014",
@@ -29,6 +42,22 @@ app.controller('MainController', ['$scope', '$http', function($scope, $http) {
 		"location": "Blagnac IUT, France"
 	}]
 
+	/*
+	This is a tad more complete than the school part.
+	Basically in there you'll put every work experience you've had.
+	Each of them will have information available as such:
+	
+	{
+		"contract": "Type of contract, like an Internship",
+		"dates": "Same as for school, the dates between which you've worked under this contract, like 2012-2013",
+		"role": "Your role in the company during this contract, Web Developer for example",
+		"company": "The company you worked in, might be Microsoft",
+		"location": "Where you worked, as in a city, state format, for example in Denver, CO",
+		"techs": "Which techs you used. This is a bit developer-specific and might not apply to you, don't bother putting it if you don't feel like it helps. Otherwise, for example PHP, HTML5, CSS3",
+		"url": "The website of the company you worked in, or of the project you worked on. http://microsoft.com/",
+		"displayUrl": "A nice, clean version of the website URL, easily readable by humans, like microsoft.com",
+		"tasks": "This is the long part. Basically put in the different tasks you did during the contract. You can use \n to put a blank line. I'll make this work with html code later on"
+	*/
 	$scope.work = [{
 		"contract": "Ten-week internship",
 		"dates": "2014",
@@ -63,6 +92,26 @@ app.controller('MainController', ['$scope', '$http', function($scope, $http) {
 		"tasks": "Following the tasks done during the apprenticeship contract\nWork distribution to other developers\nSupervision and validation of other developers' work\nManaging software deployment process, branch merging, production pushes"
 	}]
 
+	/*
+	Skills are a complex yet simple type of data here
+	What you want to do is separate your skills in categories (Web development, Microsoft Suite, Personal skills, ...)
+	and fill it as such. Here is the format:
+	
+	{
+		"category": "The category of skills, such as Web Development",
+		"skills": [{ 
+			"name": "A skill name, like PHP",
+			"level": "The skill level. This is currently rated in stars (out of 3) but will probably change. Like ***"
+		},
+		{
+			"name": "Another skill, like HTML5",
+			"level": "This skill's level, like **"
+		}]
+	}
+	
+	You can put in as many categories as you like, with as many skills as you like.
+	Be crative, that's the important stuff!
+	*/
 	$scope.skills = [{
 		"category": "Object-Oriented Programming",
 		"skills": [{
@@ -128,6 +177,21 @@ app.controller('MainController', ['$scope', '$http', function($scope, $http) {
 		}]
 	}];
 
+	/*
+	This is one of the two "misc" parts!
+	This thing right here is about *you*. Not what you can do, not what you know, who you are. What you like!
+	Basically if you're into say, hockey, you can put that here. Drawing? Yeah, sure, go ahead!
+	Keep in mind that you might get questions about this stuff because it can be considered things you /really/ like.
+	So, dont just put "movies" if you watch one every month or so, except if it's really out of real interest in specific movies!
+	
+	Well I'm not your mother, do what you want here. Just do it like so:
+	{
+		"title": "Your interest"
+	}
+	
+	Here too, put in as many as you want. If I were to say, I would advise about 5 should be a ceiling.
+	But again, I'm not your mother.
+	*/
 	$scope.likes = [{
 		"title": "Traveling"
 	},
@@ -138,6 +202,18 @@ app.controller('MainController', ['$scope', '$http', function($scope, $http) {
 		"title": "Photography"
 	}];
 
+	/*
+	The second misc part!
+	This is the classic "which languages can I speak, and how good?" part of a resume.
+	
+	The format comes in its classic way as well:
+	{
+		"name": "The language you know, like Spanish",
+		"level": "How well you master it, like School Level or Native or Bilingual or Professional. This is not exhaustive."
+	}
+	
+	Here again, put as many as you like. Depending on your job you might want 2 or 10 of those.
+	*/
 	$scope.languages = [{
 		"name": "French",
 		"level": "Native"
@@ -147,17 +223,3 @@ app.controller('MainController', ['$scope', '$http', function($scope, $http) {
 		"level": "Bilingual"
 	}];
 }]);
-
-/*
-	{
-		"category": "",
-		"skills": [{
-			"name": "",
-			"level": "***"
-		},
-		{
-			"name": "",
-			"level": "***"
-		}]
-	}
-*/
